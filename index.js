77aa07c43c7ac758c1e45ed18e7c2a369029394d
pagnkelly <632228198@qq.com>
2022-10-12 11:19:43 +0800

    feat: remove ts

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
 module.exports = {
-  parserOptions: {
-    parser: '@typescript-eslint/parser'
-  },
   extends: ['eslint:recommended', 'plugin:mpx/mpx-essential'],
   rules: {
     camelcase: ['error', { 'allow': ['__mpx_mode__', '__mpx_env__'] }],
     'mpx/script-setup-uses-vars': 2
   },
   overrides: [
-    {
-      files: ['**/*.ts'],
-      parser: '@typescript-eslint/parser',
-      extends: [
-        'standard',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-      ],
-      plugins: ['@typescript-eslint']
-    },
     {
       files: ['**/*.mpx'],
       rules: {
