8dd6d5636c4bf4917a0d55a80c23442c78267cba
pagnkelly <632228198@qq.com>
2023-04-03 11:05:13 +0800

    feat: 增加wxs规则

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -38,6 +38,12 @@ module.exports = {
       extends: [
         'eslint:recommended'
       ]
+    },
+    {
+      files: ['**/*.wxs'],
+      rules: {
+        'object-shorthand': ['error', 'never']
+      }
     }
   ]
 }
